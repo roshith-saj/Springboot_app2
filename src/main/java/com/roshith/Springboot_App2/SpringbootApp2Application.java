37e0d8518a052097fd23f759fdce9908d18f140d
Roshith <60063748+roshith-saj@users.noreply.github.com>
2024-05-30 22:13:07 +0530

    first commit

---
+++ src/main/java/com/roshith/Springboot_App2/SpringbootApp2Application.java
@@ -0,0 +1,13 @@
+package com.roshith.Springboot_App2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/roshith/Springboot_App2/SpringbootApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshith.Springboot_App2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
